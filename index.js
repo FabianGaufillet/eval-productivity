fd24222144ffe48d11a232199955a60ef86efe0a
git user <user@gmail.com>
2024-09-02 09:34:21 +0200

    Documenter le code

--- index.js
+++ index.js
@@ -7,23 +7,44 @@ const filename=fileURLToPath(import.meta.url)
 const dirname=path.dirname(filename)
 const port=8070
 const host='127.0.0.1'
-
 const app=express()
+
+/**
+ * Middleware intégré à Express.
+ * Permet d'extraire le corps d'une requête POST ou PUT, accessible par la suite dans 'req.body' sous la forme d'un objet.
+ */
 app.use(express.urlencoded({extended: true}))
+
+/**
+ * Middleware autorisant le 'Cross-Origin Resource Sharing'.
+ * Tel que défini, ici on autorise la connexion à notre serveur depuis n'importe quelle origine.
+ */
 app.use(cors())
+
+/**
+ * Middleware indiquant les répertoires de fichiers statiques auxquels Express peut accéder.
+ */
 app.use(express.static(path.join(dirname, 'public')))
 app.use('/favicon.ico', express.static(path.join(dirname, 'public', 'images', 'favicon.png')))
 
-
+/**
+ * Définit la route '/' pour la méthode 'get'
+ */
 app.get('/',(req,res)=>{
   res.sendFile('index.html',{root: path.join(dirname)},err=>{
     if (err) throw new Error(err)
   })
 })
 
+/**
+ * Définit la route '/comment' pour la méthode 'post'
+ */
 app.post('/comment',(req, res)=>{
   const comment = req.body.message
   res.send(comment)
 })
 
+/**
+ * Démarre le serveur
+ */
 app.listen(port,host,() => {console.info(`Server started @ http://${host}:${port}.`)})
--- public/js/functionsToTest.js
+++ public/js/functionsToTest.js
@@ -1,3 +1,9 @@
+/**
+ * Place les arguments dans un objet avec pour clé la position de l'argument dans la liste des arguments
+ * @function returnAnObject
+ * @param {...*} args Arguments passés à la fonction
+ * @returns {Object<*>|String} Un objet contenant les paramètres passés en argument ou un message d'information en cas d'absence d'arguments
+ */
 const returnAnObject = (...args) => {
   let response = {};
   if (args.length) {
@@ -12,6 +18,12 @@ const returnAnObject = (...args) => {
   return response;
 };
 
+/**
+ * Multiplie par 2 tous les nombres contenus dans le tableau passé en argument
+ * @function multiplyAllByTwo
+ * @param {number[]} arrayOfNumbers Un tableau de nombres
+ * @returns {number[]|string} Un tableau de nombres ou un message d'information si l'argument fourni est incorrect
+ */
 const multiplyAllByTwo = (arrayOfNumbers) => {
   let response;
   if (arrayOfNumbers.constructor.prototype === new Array().constructor.prototype) {
