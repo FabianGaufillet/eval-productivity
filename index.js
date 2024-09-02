c5c0c92a1b6d7e1a460dd11e6954863cb3137210
git user <user@gmail.com>
2024-09-02 10:25:57 +0200

    Embellir le code

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
-import express from 'express'
-import path from 'path'
-import {fileURLToPath} from 'url'
-import cors from 'cors'
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
 
-const filename=fileURLToPath(import.meta.url)
-const dirname=path.dirname(filename)
-const port=8070
-const host='127.0.0.1'
-const app=express()
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const port = 8070;
+const host = "127.0.0.1";
+const app = express();
 
 /**
  * Middleware intégré à Express.
  * Permet d'extraire le corps d'une requête POST ou PUT, accessible par la suite dans 'req.body' sous la forme d'un objet.
  */
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 /**
  * Middleware autorisant le 'Cross-Origin Resource Sharing'.
  * Tel que défini, ici on autorise la connexion à notre serveur depuis n'importe quelle origine.
  */
-app.use(cors())
+app.use(cors());
 
 /**
  * Middleware indiquant les répertoires de fichiers statiques auxquels Express peut accéder.
  */
-app.use(express.static(path.join(dirname, 'public')))
-app.use('/favicon.ico', express.static(path.join(dirname, 'public', 'images', 'favicon.png')))
+app.use(express.static(path.join(dirname, "public")));
+app.use(
+  "/favicon.ico",
+  express.static(path.join(dirname, "public", "images", "favicon.png")),
+);
 
 /**
  * Définit la route '/' pour la méthode 'get'
  */
-app.get('/',(req,res)=>{
-  res.sendFile('index.html',{root: path.join(dirname)},err=>{
-    if (err) throw new Error(err)
-  })
-})
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(dirname) }, (err) => {
+    if (err) throw new Error(err.message);
+  });
+});
 
 /**
  * Définit la route '/comment' pour la méthode 'post'
  */
-app.post('/comment',(req, res)=>{
-  const comment = req.body.message
-  res.send(comment)
-})
+app.post("/comment", (req, res) => {
+  const comment = req.body.message;
+  res.send(comment);
+});
 
 /**
  * Démarre le serveur
  */
-app.listen(port,host,() => {console.info(`Server started @ http://${host}:${port}.`)})
+app.listen(port, host, () => {
+  console.info(`Server started @ http://${host}:${port}.`);
+});
--- public/js/functionsToTest.js
+++ public/js/functionsToTest.js
@@ -8,13 +8,13 @@ const returnAnObject = (...args) => {
   let response = {};
   if (args.length) {
     let index = 0;
-    args.forEach(arg => {
+    args.forEach((arg) => {
       response[index] = arg;
       index++;
-    })
+    });
   } else {
-    response = 'No argument was given to the function.';
-  };
+    response = "No argument was given to the function.";
+  }
   return response;
 };
 
@@ -26,11 +26,13 @@ const returnAnObject = (...args) => {
  */
 const multiplyAllByTwo = (arrayOfNumbers) => {
   let response;
-  if (arrayOfNumbers.constructor.prototype === new Array().constructor.prototype) {
-    response = arrayOfNumbers.map((val => val * 2));
-    console.log('arrayTimesTwo: ', response);
+  if (
+    arrayOfNumbers.constructor.prototype === new Array().constructor.prototype
+  ) {
+    response = arrayOfNumbers.map((val) => val * 2);
+    console.log("arrayTimesTwo: ", response);
   } else {
-    response = 'The argument is not an Array of numbers';
-  };
+    response = "The argument is not an Array of numbers";
+  }
   return response;
 };
